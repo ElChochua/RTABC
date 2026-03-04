879a75051606a9cc0f0145727fa9a0c1fe66a9c0
ElChochua <jossue.avll@gmail.com>
2026-03-04 07:57:11 -0700

    testing Enigo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use eframe::egui;
 use tokio::task;
-
 mod app;
+use enigo::{
+    Button, Coordinate,
+    Direction::{Click, Press, Release},
+    Enigo, Key, Keyboard, Mouse, Settings,
+};
+
 #[tokio::main]
 async fn main() {
-    let v = vec![1,2,3];
     env_logger::init();
+
     let native_options = eframe::NativeOptions{
         viewport: egui::ViewportBuilder::default().with_inner_size([900.0,600.0]).
             with_min_inner_size([500.0,300.0]),
@@ -15,8 +20,5 @@ async fn main() {
                        native_options,
                        Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
     ).expect("TODO: panic message");
-    //Just testing the tokio library
-    task::spawn(async move {
-        println!("vec {:?}", v);
-    });
+    
 }
